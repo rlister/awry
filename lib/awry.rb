4e210d8abd4737e301ff99c038d8c01c5f6cd57e
Richard Lister <rlister@gmail.com>
2021-09-09 17:00:29 -0400

    lib

---
+++ lib/awry.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "awry/version"
+
+module Awry
+  MSG = 'wibble'
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/awry/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Awry
+  VERSION = "0.1.0"
+end
