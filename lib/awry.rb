38eea3dbd7ad8c13ec150f6c590a0d4b89edc2ad
Richard Lister <rlister@gmail.com>
2021-09-11 15:56:19 -0400

    base cli class

--- lib/awry.rb
+++ lib/awry.rb
@@ -4,4 +4,19 @@ require_relative "awry/version"
 
 module Awry
   class Error < StandardError; end
+
+  class Cli < Thor
+
+    no_commands do
+      def color(string)
+        set_color(string, self.class::COLORS.fetch(string.to_sym, :yellow))
+      end
+
+      def tag_name(thing, default = nil)
+        tn = thing.tags.find { |tag| tag.key == 'Name' }
+        tn ? tn.value : default
+      end
+    end
+
+  end
 end
